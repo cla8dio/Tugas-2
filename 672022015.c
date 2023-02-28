cafbeb6f52a8dc60a2a7f0cf08bb349d147815d7
Claudio Canavaro <123723217+cla8dio@users.noreply.github.com>
2023-02-28 15:17:50 +0700

    Add files via upload
    Final

--- 672022015.c
+++ 672022015.c
@@ -2,6 +2,125 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <math.h>
+#include <string.h>
+
+char pw1,uname1[100];
+char password1[100] ="";
+
+
+char pw2,uname2[100];
+char password2[100] ="";
+
+
+int coba = 2;
+
+
+void regis() {
+
+    char username[100];
+    char pw[100];
+    int i = 0, x = 10;
+
+    printf(" Sign Up \n");
+    printf("Username : ");
+    scanf(" %[^\n]s",&uname1);
+    printf("Password : ");
+    //scanf(" %[^\n]s",&pw);
+    //fflush(stdin);
+
+   // strcpy(password1,pw);
+    //strcpy(uname1,username);
+
+    while((pw1=(char)_getch()) != '\r' ){
+        if(pw1==8){
+            password1[i]='\0';
+            printf(" ");
+            i--;
+            x--;
+        } else{
+            password1[i] = pw1;
+            printf("*");
+            x++;
+            i++;
+        }
+    }
+
+    printf("\n");
+
+    system("cls");
+    printf("Pendaftaran berhasil!");
+    getch();
+    system("cls");
+    login();
+
+
+}
+
+void login() {
+
+     //====LOGIN=====
+
+char username[100];
+    char pw[100];
+    int j = 0, y = 10;
+
+
+    printf(" LOGIN \n");
+    printf("Username :");
+    scanf(" %[^\n]s",&uname2);
+    printf("Password :");
+
+   // scanf(" %[^\n]s",&password2);
+
+
+	while((pw2=(char)_getch()) != '\r' ){
+        if(pw2==8){
+            password2[j]='\0';
+            printf(" ");
+            j--;
+            y--;
+        } else{
+            password2[j] = pw2;
+            printf("*");
+            y++;
+            j++;
+        }
+    }
+
+    //fflush(stdin);
+
+    //strcpy(password2,pw);
+    //strcpy(uname2,username);
+
+
+    printf("\n");
+
+
+
+    if((strcmp(uname1,uname2)==0) && (strcmp(password1,password2)==0) && (coba >= 0)) {
+        printf("login berhasil");
+        getch();
+        system("cls");
+        menu();
+    } else if(strcmp(uname1,uname2)!=0 || strcmp(password1,password2)!=0 && coba >= 0){
+        printf("Password atau Username salah!\nAnda masih punya %d kesempatan", coba);
+        coba--;
+        getch();
+
+        system("cls");
+        login();
+    } else {
+        printf("");
+    }
+
+
+
+}
+
+
+//==================tugas2=====================
+
+
 
 //=========================================MENU 1==================================================
 
@@ -712,6 +831,10 @@ void menu() {
         printf("\n\n   TERIMA KASIH :)\n\n");
     } else {
         printf("Pilihan anda tidak ada di menu :(");
+        fflush(stdin);
+        getch();
+        system("cls");
+        menu();
     }
 
 }
@@ -719,7 +842,7 @@ void menu() {
 int main()
 {
     system("COLOR f1");
-    menu();
+    regis();
     return 0;
 }
 
